21cfd812951f02c6afb2403ccfddccb099be8de8
TheNexusAvenger <zpcook@googlemail.com>
2016-07-31 00:29:33 -0400

    Added default encoding settings

--- js/helpers/songutil.js
+++ js/helpers/songutil.js
@@ -195,7 +195,11 @@ function PlayRandomSong(){
 	GenreColor = GetColorFromGenre(GenreName)
   CreateNewFleck()
 
-  DownloadSongData = false
+  if (EncodeEnabledByDefault == true) {
+    DownloadSongData = true
+  } else {
+    DownloadSongData = false
+  }
 
   var SongBackgroundOverride = SongBackgrounds[SingleLineSongName]
   var AlbumBackgroundOverride = AlbumBackgrounds[Album]
@@ -291,7 +295,7 @@ function InitializeSpectrumHandler() {
   SongOrder = GetRandomTableOfNumbers(Songs.length)
   AudioNode.onaudioprocess = HandleAudio
   Analyser.fftSize = FFTSize
-  Analyser.smoothingTimeConstant = 0.20
+  Analyser.smoothingTimeConstant = 0.2
   GainNode.gain.value = 0;
   GainNode.connect(Context.destination)
   AudioNode.connect(Context.destination)
