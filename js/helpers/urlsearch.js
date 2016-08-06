a293ebfd0c7c75393819f5fce011635e2479a489
TheNexusAvenger <zpcook@googlemail.com>
2016-08-06 00:31:42 -0700

    Added URL search functionality

---
+++ js/helpers/urlsearch.js
@@ -0,0 +1,31 @@
+//From https://www.creativejuiz.fr/blog/en/javascript-en/read-url-get-parameters-with-javascript
+
+function $_GET(param) {
+	var vars = {};
+	window.location.href.replace(location.hash, '' ).replace(
+		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+		function( m, key, value ) { // callback
+			vars[key] = value !== undefined ? value : '';
+		}
+	);
+
+	if ( param ) {
+		return vars[param] ? vars[param] : null;
+	}
+	return vars;
+}
+
+
+var SongNameSearch = ""
+var ArtistNameSearch = ""
+
+var WantedSongName = $_GET("song")
+var WantedArtistName = $_GET("artist")
+
+if (WantedSongName) {
+  SongNameSearch = decodeURIComponent(WantedSongName.toLowerCase())
+}
+
+if (WantedArtistName) {
+  ArtistNameSearch = decodeURIComponent(WantedArtistName.toLowerCase())
+}
