ba448846bf883001ab1402c20e11c9bd85d99c03
TheNexusAvenger <zpcook@googlemail.com>
2016-08-19 19:51:17 -0400

    Added 3 new albums

--- js/songdata/albums.js
+++ js/songdata/albums.js
@@ -39,12 +39,15 @@ RegisterAlbum("buildthecitiesreconstructions","Karma Fields - Build The Cities (
 RegisterAlbum("monstercat5yearanniversary","Monstercat - Monstercat 5 Year Anniversary",["Matches (feat. Aaron Richards)","Lift You Up (feat. EMEL)","Blackout","Divided VIP","Imperfect Views","This Feeling","Tribal","Break The Silence","Crescendo (feat. MLYK)","Before We Fade"])
 
 RegisterAlbum("basslinekickintheremixes","Pegboard Nerds - Bassline Kickin (The Remixes)",["Bassline Kickin (Astronaut Remix)","Bassline Kickin (Dzeko & Torres Remix)","Bassline Kickin (Silverback Remix)"])
+RegisterAlbum("heartbittheremixes","Pegboard Nerds - Heartbit (The Remixes)",["Heartbit (feat. Tia Simone) (12th Planet Remix)","Heartbit (feat. Tia Simone) (Sikdope Remix)","Heartbit (feat. Tia Simone) (Tommie Sunshine X Usica Remix)","Heartbit (feat. Tia Simone) (MIU Remix)","Heartbit (feat. Tia Simone) (Quiet Disorder Remix)","Heartbit VIP (feat. Tia Simone)"])
 RegisterAlbum("pinkcloudep","Pegboard Nerds - Pink Cloud EP",["Emoji","Pink Cloud (feat. Max Collins)","Just Like That (feat. Johnny Graves)","Downhearted (feat. Jonny Rose)","The End Is Near (Fire In The Hole VIP)"])
 RegisterAlbum("thelosttracksep","Pegboard Nerds - The Lost Tracks EP",["Frainbreeze","Close Encounter","20K","Lawless","Revenge Of The Nerds (VIP Mix)","Rocktronik"])
+RegisterAlbum("theuncagedremixes","Pegboard Nerds - The Uncaged Remixes",["Here It Comes (Snavs & Toby Green Remix)","Badboi (Snavs Remix)","Badboi (Dani Deahl Remix)","Badboi (VIP)"])
 
 RegisterAlbum("dreamsep","Rogue - Dream EP",["Dreams (feat. Laura Brehm)","Air","Moments (feat. Meg Dean)","Escape"])
 RegisterAlbum("earthep","Rogue - Earth EP",["From The Dust","Cataclysm (feat. Meg Dean)","Perfect Views","Through The Dark"])
 
 RegisterAlbum("dancetoep","Tut Tut Child - Dance To It EP",["Dance To It","Dragon Pirates","Fat Cat Adventures"])
+RegisterAlbum("askyourfriendsfirstep","Tut Tut Child - Ask Your Friends First EP",["Hot Pursuit","Breathe (feat. Danyka Nadeau)","Plain Sight (feat. Rachel Hirons)","Birds On The Wire (feat. Augustus Ghost)"])
 
 RegisterAlbum("miraisekaiep","Varien & 7 Minutes Dead - Mirai Sekai EP",["Mirai Sekai Pt.1: Neo-Seoul","Mirai Sekai Pt.2: Neo-Tokyo","Mirai Sekai Pt.3: Aeon Metropolis","Mirai Sekai Pt.4: Goodnight_Sequence","Mirai Sekai (Continuous Mix)"])
