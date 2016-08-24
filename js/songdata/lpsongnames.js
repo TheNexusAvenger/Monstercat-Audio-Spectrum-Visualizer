57f6ba99736ea3572e265094a5c5eb33b45e080a
TheNexusAvenger <zpcook@googlemail.com>
2016-08-24 18:33:15 -0400

    Create lpsongnames.js

---
+++ js/songdata/lpsongnames.js
@@ -0,0 +1,5 @@
+function AddLPSongData (SongName,Data) {
+  LPSongNames[SongName] = Data
+}
+
+AddLPSongData("Mirai Sekai (Continuous Mix)",[[0,"Varien &<br>7 Minutes Dead","Mirai Sekai Pt. 1: Neo-Seoul"],[210000,"Varien &<br>7 Minutes Dead","Mirai Sekai Pt. 2: Neo-Tokyo"],[472000,"Varien &<br>7 Minutes Dead","Mirai Sekai Pt. 3: Aeon Metropolis"],[708000,"Varien &<br>7 Minutes Dead","Mirai Sekai Pt. 4: Goodnight Sequence"]])
